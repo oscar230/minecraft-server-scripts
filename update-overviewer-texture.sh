a05761d83f4a4bb68dd666d4d16f414c29060ec8
Oscar Andersson <2kafkimc@duck.com>
2022-10-12 21:42:02 +0200

    Create update-overviewer-texture.sh

---
+++ update-overviewer-texture.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION_MANIFEST_URL=https://launchermeta.mojang.com/mc/game/version_manifest.json
+
+if ! command -v curl &> /dev/null
+then
+    echo "curl could not be found"
+    exit
+fi
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit
+fi
+
+VERSION_MANIFEST=$(curl -s $VERSION_MANIFEST_URL)
+VERSION=$(echo $VERSION_MANIFEST | jq ".latest.release" | sed 's/"//g')
+TEXTURE_FILE=texture-${VERSION}.jar
+TEXTURE_FILE_LINK=texture-latest.jar
+
+if [ -f "$TEXTURE_FILE" ];
+then
+    echo "The $TEXTURE_FILE_LINK is up to date at version $VERSION."
+else 
+    echo "The $TEXTURE_FILE does not exist, will get the latest release now."
+    curl --output "$TEXTURE_FILE" -s https://overviewer.org/textures/${VERSION}
+    echo "Updated $TEXTURE_FILE to version $VERSION"
+fi
+
+ln -f -s $TEXTURE_FILE $TEXTURE_FILE_LINK
